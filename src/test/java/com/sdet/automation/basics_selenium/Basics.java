c912c0a05ba414b06036733c86400b6a97bb6cf6
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2026-01-24 13:41:30 +0530

    Added launching browser code with switch case

--- src/test/java/com/sdet/automation/basics_selenium/Basics.java
+++ src/test/java/com/sdet/automation/basics_selenium/Basics.java
@@ -2,13 +2,33 @@ package com.sdet.automation.basics_selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Basics {
 
     @Test
     public void launchBrowser(){
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = null;
+
+        String baseURL = "https://www.google.com";
+        String browser = "chrome";
+
+        switch (browser){
+            case "chrome":
+                driver = new ChromeDriver();
+                driver.get(baseURL);
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                driver.get(baseURL);
+                break;
+            default:
+                System.err.println("Browser Not Found");
+        }
+
+
+
     }
 
 }
